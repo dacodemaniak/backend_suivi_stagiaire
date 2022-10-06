2930d443d42127a00bf197bdee231bde12ca9374
DaCodeManiaK <jla.webprojet@gmail.com>
2022-10-06 09:58:06 +0200

    ADD: Request and Response for User DTO Helpers

---
+++ src/main/java/com/aelion/suivi/dto/UserRequest.java
@@ -0,0 +1,29 @@
+package com.aelion.suivi.dto;
+
+import java.util.List;
+
+public class UserRequest {
+	private String userName;
+	private String userPass;
+	private List<String> roles;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPass() {
+		return userPass;
+	}
+	public void setUserPass(String userPass) {
+		this.userPass = userPass;
+	}
+	public List<String> getRoles() {
+		return roles;
+	}
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	
+	
+}
---
+++ src/main/java/com/aelion/suivi/dto/UserResponse.java
@@ -0,0 +1,27 @@
+package com.aelion.suivi.dto;
+
+import java.util.List;
+
+public class UserResponse {
+	private String token;
+	
+	private List<String> roles;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	
+	
+}
