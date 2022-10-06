fe695bc0230510fde11380c4cd54f38d21336006
DaCodeManiaK <jla.webprojet@gmail.com>
2022-10-06 10:40:56 +0200

    ADD: DTO for Signup message

---
+++ src/main/java/com/aelion/suivi/dto/SignupMessage.java
@@ -0,0 +1,13 @@
+package com.aelion.suivi.dto;
+
+public class SignupMessage {
+	private String message;
+	
+	public SignupMessage(String message) {
+		this.message = message;
+	}
+	
+	private String getMessage() {
+		return this.message;
+	}
+}
